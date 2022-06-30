5f9b6668fbd51f3aaa08dc424614917be63a755b
Maro189 <91327484+Maro189@users.noreply.github.com>
2022-06-30 16:19:52 +0400

    Source Code

--- EGYPT GDP 2010-2021.py
+++ EGYPT GDP 2010-2021.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 # importing packages
@@ -11,7 +11,7 @@ import matplotlib as plt
 import seaborn as sns
 
 
-# In[ ]:
+# In[2]:
 
 
 # defining data frame and opening csv file with pandas
@@ -20,72 +20,36 @@ df1 = pd.read_csv(r'G:\Python projects\Egypt GDP\df1.csv')
 df1
 
 
-# In[ ]:
+# # Dataframe manipulation
 
-
-# droping indicies 0-1 and redefining dataframe
-
-df1 = df1.drop(range(0,5))
-df1
-
-
-# In[ ]:
+# In[3]:
 
 
 # dropping years 1960-2009 as they don't sreve a purpose for this analysis
-
 df1 = df1.drop(df1.columns.to_series()["1960":"2009"], axis=1)
-df1
 
-
-# In[ ]:
-
+# droping indicies 0-4
+df1 = df1.drop(range(0,6))
 
 # dropping indicator code
-
-df1 = df1.drop(columns = "Indicator Code")
-df1
-
-
-# In[ ]:
-
-
-# dropping index 5
-
-df1 = df1.drop(5)
-df1
-
-
-# In[ ]:
-
+df1 = df1.drop(columns = "Indicator Code",)
 
 # dropping index 8, not enough data to include in the analysis
-
 df1 = df1.drop(8)
-df1
-
-
-# In[ ]:
-
 
 # resetting index for better clarification
-
 df1 = df1.reset_index(drop=True)
-df1
-
-
-# In[ ]:
-
 
 df1
 
 
-# In[ ]:
+# In[4]:
 
 
 import random
 from matplotlib import pyplot as plt 
 import numpy as np 
+import seaborn as sns
 from matplotlib.animation import FuncAnimation 
 
 labels = ['Agricultural', 'Industrial', 'Services']
@@ -99,7 +63,7 @@ data = np.array([[13.340759, 35.784518, 46.232720],
  [11.485285, 33.750764, 53.240873],
  [11.225000, 35.623446, 50.471695],
  [11.567609, 32.009145, 51.760507]])
-# explode = (0, 0.1, 0, 0) 
+
 years = [2010 + i  for i in range(11)]
 
 plot_steps = np.array([data[0]])
@@ -119,27 +83,21 @@ for i, step in enumerate(plot_steps):
     if i % 100 == 0:
         year = next(years)
     plt.xlim(0, 100)
-    plt.barh(labels, step, color = ["green", "blue", "red"])
+    plt.barh(labels, step, color = sns.color_palette("tab10"))
     plt.text(0.8, 0.2, year, fontsize = 14)
     plt.pause(0.01)
     plt.clf()
 plt.show()
 
 
-# In[ ]:
+# In[7]:
 
 
 df2 = pd.read_csv(r'G:\Python projects\Egypt GDP\GDP-Year.csv')
 df2
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
+# In[6]:
 
 
 import matplotlib.pyplot as plt
